fe617760dea375406a7d66f1ab44c7bcc9948c5f
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-25 16:43:09 -0400

    Added resolvers and typedef files

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,28 @@
+const { User, Book } = require('../models');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id }).populate('thoughts');
+        }
+        throw new AuthenticationError('You need to be logged in!');
+      },
+  },
+  Mutation: {
+    createMatchup: async (parent, args) => {
+      const matchup = await Matchup.create(args);
+      return matchup;
+    },
+    createVote: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookID: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+      
+  }
+
+  type Query {
+      me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors [String], description: String!, title: String!, bookID: String!, image: String, link: String): User
+    removeBook(bookID: String!): User
+  }
+`;
+
+module.exports = typeDefs;
