284abf9ab580378e96a7b64523a92759aa2e5d58
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-28 18:20:45 -0400

    Removed Assets folder and removed Develop folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: String!
+    bookID: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input bookInput {
+    authors: [String]
+    description: String
+    title: String
+    bookID: String
+    image: String
+    link: String
+  }
+
+  type Query {
+      me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput!): User
+    removeBook(bookID: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
