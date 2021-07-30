4f137a29aaad63fa7ff0cdfff8e68220a377bcdc
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-30 14:42:17 -0400

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+const mongoose = require("mongoose");
 // const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 
