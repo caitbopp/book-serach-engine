210ecccaa96b73957348f3d7d003f08a18dc0ed8
Caitlin Bopp <boppcaitlin@gmail.com>
2021-07-30 14:06:56 -0400

    Fixed REMOVE_BOOK mutation in mutation.js file client side

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -44,7 +44,20 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
-   removeBook(bookId: $bookId)
+  mutation removeBook($bookId: String!) {
+   removeBook(bookId: $bookId) {
+     _id
+     username
+     email
+     bookCount
+     savedBooks {
+       bookId
+       authors
+       image
+       description
+       title
+       link
+     }
+   }
   }
 `;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,11 +47,11 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
         },
-        removeBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
                     { new: true }
                   );
 
